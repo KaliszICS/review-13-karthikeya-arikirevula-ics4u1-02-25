ff46c52732f498397b72ef5f845ce575807898ba
karthikeya-arikirevula-ics4u1-02-25 <karthikeya.arikirevula@student.tdsb.on.ca>
2025-03-04 15:06:58 +0000

    finish

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,4 +1,5 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;;
 
 public class PracticeProblem {
 
@@ -6,4 +7,40 @@ public class PracticeProblem {
 
 	}
 
+	public static ArrayList<String> swap(ArrayList<String> lists, int nums, int num2) {
+		if (nums >= 0 && num2 >= 0 && nums < lists.size() && num2 < lists.size()) {
+			String tempString = lists.get(nums);
+			lists.set(nums, lists.get(num2));
+			lists.set(num2, tempString);
+		}
+		return lists;
+	}
+
+	public static ArrayList<Double> createArrayList(double[] secondArray) {
+		ArrayList<Double> secondList = new ArrayList<>();
+
+		for (int i = 0; i < secondArray.length; i++) {
+			secondList.add(secondArray[i]);
+		}
+		return secondList;
+	}
+
+	public static HashMap<String, Integer> combineParallelArrays(String[] name, int[] age) {
+		HashMap<String, Integer> dora = new HashMap<>();
+
+		for (int i = 0; i < name.length; i++) {
+			dora.put(name[i], age[i]);
+		}
+
+		return dora;
+	}
+
+	public static HashMap<String, Integer> increaseAge(HashMap<String, Integer> dora, String name) {
+		if (dora.containsKey(name)) {
+			dora.put(name, dora.get(name) + 1);
+		}
+
+		return dora;
+	}
 }
+
